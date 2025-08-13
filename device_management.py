a0aee19070936e918e974e154ac1daaab3869ae2
noam007 <noamlevi007@gmail.com>
2025-08-13 22:27:06 +0300

    new runner file

--- device_management.py
+++ device_management.py
@@ -44,6 +44,18 @@ async def delete_devices(redis_client):
         print(key)
         await redis_client.delete(key)
 
+async def get_names(redis_client):
+    device_keys = []
+    async for key in redis_client.scan_iter(match="device:*"):
+        device_keys.append(key)
+
+    if not device_keys :
+        print("there are no devices in Redis")
+    else:
+        print(device_keys)
+
+
+
 async def create_device(redis_client):
     """Creates a single device in Redis and cleans it up after the test."""
     device_id = "test-device-456"  # You can use a static ID or generate a new one
@@ -59,19 +71,38 @@ async def create_device(redis_client):
     await redis_client.hset(f"device:{device_id}", mapping=device_data)
 
 
+async def add_comment_to_device(redis_client, device_id, comment):
+    print(device_id,comment)
+    # await redis_client.hset(f"device:{device_id}", key="comment", value=comment)
+    await redis_client.hset(f"device:{device_id}", key="comment", value="hello_commensdfsdft")
+    print(f"Comment added to device '{device_id}'.")
+
+
+
+
+
+
+
 async def main():
     redis_client = redis.Redis(host="localhost", port=6379, db=0, decode_responses=True)
     await redis_client.ping()
     print("Connected to Redis successfully!")
 
+
     ### create singe device
-    await create_device(redis_client)
+    # await create_device(redis_client)
 
     ### create singe device
     # await setup_devices(redis_client)
 
     ### delete devices
     # await delete_devices(redis_client)
+
+    ### get all Redis devices names:
+    # await get_names(redis_client)
+
+    ### add a Comment to device:
+    await add_comment_to_device(redis_client, "test-device-456", "hello world")
 
 if __name__ == "__main__":
     asyncio.run(main())
---
+++ runner.py
@@ -0,0 +1,8 @@
+import uvicorn
+import requests
+
+
+if __name__ == "__main__":
+    uvicorn.run("app:app", host="0.0.0.0", port=8000, reload=True)
+    print("Server is not responding !")
+
